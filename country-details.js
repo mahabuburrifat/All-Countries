299c5a3873b67d941402638c66b328f8b5f66e5e
mahabuburrifat <mahabubur06528@gmail.com>
2025-11-26 16:41:16 +0600

    update country-details JS

--- country-details.js
+++ country-details.js
@@ -1,7 +1,45 @@
-fetch('https://restcountries.com/v3.1/all?fields=name,flags,capital,region,population,nativename,subregion,toplaveldomain,currencies,languages')
+const countryName = new URLSearchParams(location.search).get('name');
+
+const countryImage = document.querySelector('.country-details-container img')
+const countryNameH2 = document.querySelector('.all-text-container h2');
+const nativeName = document.querySelector('.native-name');
+const population = document.querySelector('.population');
+const region = document.querySelector('.region');
+const subRegion = document.querySelector('.sub-region');
+const capital = document.querySelector('.capital');
+const topLevelDomain = document.querySelector('.top-level-domain')
+const area = document.querySelector('.area');
+const currency = document.querySelector('.currency');
+const languages = document.querySelector('.language');
+
+
+fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
 .then((res) => res.json())
-.then((data) => {
-    data.forEach((country) => {
-        console.log(country)
-    })
+.then(([country]) => {
+    console.log(country);
+
+    countryImage.src = country.flags.svg;
+    countryNameH2.innerText = country.name.common;
+    if(country.name.nativeName) {
+        nativeName.innerText = Object.values(country.name.nativeName)[0].common;
+    } else {
+        nativeName.innerText = country.name.nativeName;
+    }
+    population.innerText = country.population.toLocaleString('en-BD');
+    region.innerText = country.region;
+    subRegion.innerText = country.subregion;
+    capital.innerText = country.capital;
+    topLevelDomain.innerText = country.tld;
+    area.innerText = country.area.toLocaleString('en-BD');
+    if(country.currencies) {
+        currency.innerText = Object.values(country.currencies)[0].name;
+    } else {
+        currency.innerText = country.currencies;
+    }
+
+    if(country.languages) {
+        languages.innerText = Object.values(country.languages).join(', ');
+    } else {
+        languages.innerText = country.languages;
+    }
 })
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const cardContainer = document.querySelector('.card-container');
 
-fetch ('https://restcountries.com/v3.1/all?fields=name,flags,capital,region,population')
+fetch ('https://restcountries.com/v3.1/all?fields=name,flags,capital,region,population,nativename,subregion,toplaveldomain,currencies,languages')
 .then((res) => res.json())
 .then((data) => {
     data.forEach((country) => {
         // console.log(country);
         const countryCard = document.createElement('a');
         countryCard.classList.add('country-card');
-        countryCard.href ="/country-details.html"
+        countryCard.href =`/country-details.html?name=${country.name.common}`
         countryCard.innerHTML = `
             <img src="${country.flags.svg}" alt="">
              <div class="card-container-text">
